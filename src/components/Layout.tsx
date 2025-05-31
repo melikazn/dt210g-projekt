fd6f7cdfe9c4f180f3140d3f460a3b16e1c20a7e
Melika <89937665+melikazn@users.noreply.github.com>
2025-06-01 03:18:30 +0330

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,14 +4,14 @@ import Header from "./Header";
 // Layout-komponent som fungerar som en wrapper för hela applikationen
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
+      <div className="d-flex flex-column min-vh-100">
       <Header /> {/* Inkluderar Header-komponenten högst upp på sidan */}
       
       {/* Main-del där innehållet (children) från de andra komponenterna kommer att visas */}
-      <main className="container my-5 main ">{children}</main>
+      <main className="container my-5 main flex-grow-1">{children}</main>
 
       {/* Footer som visas längst ner på sidan */}
-      <footer className="text-center p-3 text-bg-dark text-white">
+      <footer className="text-center p-3 text-bg-dark text-white mt-auto">
         <p>© 2025 Min Filmapp</p> {/* Copyrighttext */}
       </footer>
     </div>
